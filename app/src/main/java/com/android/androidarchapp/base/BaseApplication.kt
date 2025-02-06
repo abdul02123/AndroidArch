9c50d2d84e80a556c3ac7b3c7577dfd384543de7
Abdul Rehman <abdulrehman02123@gmail.com>
2025-02-06 19:12:05 +0500

    contxt

--- app/src/main/java/com/android/androidarchapp/base/BaseApplication.kt
+++ app/src/main/java/com/android/androidarchapp/base/BaseApplication.kt
@@ -5,4 +5,15 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class BaseApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        appContext = this
+    }
+
+    companion object{
+        private lateinit var appContext: BaseApplication
+        fun getInstance(): BaseApplication{
+            return appContext
+        }
+    }
 }
